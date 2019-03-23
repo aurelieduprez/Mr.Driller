602b40d7f0343d1df0dbdbef97b643ffcb9b654f
Laurent Cusimano <291960@supinfo.com>
2019-03-23 16:47:02 +0100

    Added Pycharm Project

---
+++ Mr. Driller/level.py
@@ -0,0 +1,20 @@
+def generateLvl(colors, lines, width):    # This returns a 3D array [[[color, state]
+
+    lvl = []
+    for i in range(lines):
+        line = []
+        for j in range(width):
+            block = randint(1, colors)
+
+            line.append(block)
+
+        lvl.append(line)
+
+    return lvl
+
+
+def displayLvLTxt(lvl):
+
+    for element in lvl:
+        print(element)
+
---
+++ Mr. Driller/main.py
@@ -0,0 +1,9 @@
+from random import randint
+import pygame, sys
+from pygame.locals import *
+
+def game(x, y):
+    pygame.init()
+    maSurface = pygame.display.set_mode((500,300))
+
+
