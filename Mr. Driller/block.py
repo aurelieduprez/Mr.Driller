42bd6f9ddb6cba482bc7e1b6a373beea343dcbd8
Kentin <296188@supinfo.com>
2019-05-21 17:05:14 +0200

    add block chain reaction

--- Mr. Driller/block.py
+++ Mr. Driller/block.py
@@ -2,22 +2,27 @@ from os import path
 import pygame
 
 
+
 class Block:
     """General block mother-class"""
 
-    def __init__(self, posX, posY, forceHP):
+    def __init__(self, posX, posY, forceHP,chain_reaction,colors=0):
         self._posX = posX
         self._posY = posY
         self._currOffset = 0
+        if(chain_reaction):
+            self._colors = colors
 
         self._hp = forceHP
-
+        self._chain_reaction = chain_reaction
         self._texturePath = path.join("Assets", "Textures", "Blocks", "Neutral", "b_s.png")
         self._bg = path.join("Assets", "Textures", "Background", "bg.png")
 
     def hpAccess(self):
         return self._hp
 
+    def ColorAccess(self):
+        return self._colors
     def updOffset(self, currentOffset):
         self._currOffset = currentOffset
 
@@ -35,6 +40,20 @@ class Block:
 
         self._hp -= 1
         print("-1hp")
+        if(self._chain_reaction == 1):
+            from level import level
+            if(level[self._posY+1][self._posX].hpAccess() != 0):
+                if(level[self._posY+1][self._posX].ColorAccess() == self._colors):
+                    level[self._posY+1][self._posX].hit(surface)
+            if (level[self._posY - 1][self._posX].hpAccess() != 0):
+                if (level[self._posY - 1][self._posX].ColorAccess() == self._colors):
+                    level[self._posY - 1][self._posX].hit(surface)
+            if(self._posX < len(level[0])-1 and self._posX < len(level[0])-1 and level[self._posY][self._posX+1].hpAccess() != 0):
+                if (level[self._posY][self._posX+1].ColorAccess() == self._colors):
+                        level[self._posY][self._posX+1].hit(surface)
+            if (level[self._posY][self._posX-1].hpAccess() != 0):
+                if (level[self._posY][self._posX-1].ColorAccess() == self._colors):
+                    level[self._posY][self._posX-1].hit(surface)
         self.display(surface, 0, self._currOffset)
 
 
@@ -42,7 +61,8 @@ class Classic(Block):
     """Classic block daughter-class"""
 
     def __init__(self, posX, posY, colour, forceHP):
-        Block.__init__(self, posX, posY, forceHP)
+        colors=colour
+        Block.__init__(self, posX, posY, forceHP,1,colors)
         self.__colour = colour
         self._texturePath = path.join("Assets", "Textures", "Blocks", str(colour), "b_s.png")
 
@@ -51,7 +71,7 @@ class Unbreakable(Block):
     """Unbreakable block daughter-class"""
 
     def __init__(self, posX, posY):
-        Block.__init__(self, posX, posY, 5)
+        Block.__init__(self, posX, posY, 5,0)
         self._texturePath = path.join("Assets", "Textures", "Blocks", "Unbreakable", "b_s.png")
 
 
@@ -59,7 +79,7 @@ class Solo(Block):
     """Not connectable block daughter-class"""
 
     def __init__(self, posX, posY):
-        Block.__init__(self, posX, posY,1)
+        Block.__init__(self, posX, posY,1,0)
         self._texturePath = path.join("Assets", "Textures", "Blocks", "Solo", "b_s.png")
 
 
@@ -67,7 +87,7 @@ class Delayed(Block):
     """Timeout block daughter-class"""
 
     def __init__(self, posX, posY):
-        Block.__init__(self, posX, posY,1)
+        Block.__init__(self, posX, posY,1,0)
         self.__texturePath = path.join("Assets", "Textures", "Blocks", "Delayed", "b_s.png")
         self.__timeout = 84    # number of image for fadeout
 
