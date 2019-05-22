75f35b9949a7ce354e2d52605f97578477cf146e
Laurent Cusimano <291960@supinfo.com>
2019-05-22 10:25:01 +0200

    Added type  accessors in Block.py

--- Mr. Driller/block.py
+++ Mr. Driller/block.py
@@ -6,7 +6,7 @@ import pygame
 class Block:
     """General block mother-class"""
 
-    def __init__(self, posX, posY, forceHP,chain_reaction,colors=0):
+    def __init__(self, posX, posY, forceHP, chain_reaction, colors=0):
         self._posX = posX
         self._posY = posY
         self._currOffset = 0
@@ -15,6 +15,7 @@ class Block:
 
         self._hp = forceHP
         self._chain_reaction = chain_reaction
+        self._blockType = "neutral"
         self._texturePath = path.join("Assets", "Textures", "Blocks", "Neutral", "b_s.png")
         self._bg = path.join("Assets", "Textures", "Background", "bg.png")
 
@@ -23,6 +24,10 @@ class Block:
 
     def ColorAccess(self):
         return self._colors
+
+    def typeAccess(self):
+        return self._blockType
+
     def updOffset(self, currentOffset):
         self._currOffset = currentOffset
 
@@ -36,7 +41,7 @@ class Block:
             image = pygame.image.load(self._texturePath)
             surface.blit(image, (self._posX * 64 + 26, (self._posY * 64 + 12) - currentOffset*64))
 
-    def hit(self, surface,nochain = 0):
+    def hit(self, surface, nochain = 0):
 
         self._hp -= 1
         print("-1hp")
@@ -67,38 +72,44 @@ class Classic(Block):
 
     def __init__(self, posX, posY, colour, forceHP):
         colors=colour
-        Block.__init__(self, posX, posY, forceHP,1,colors)
+        Block.__init__(self, posX, posY, forceHP, 1, colors)
         self.__colour = colour
         self._texturePath = path.join("Assets", "Textures", "Blocks", str(colour), "b_s.png")
+        self._blockType = "classic"
 
 
 class Unbreakable(Block):
     """Unbreakable block daughter-class"""
 
     def __init__(self, posX, posY):
-        Block.__init__(self, posX, posY, 5,0)
+        Block.__init__(self, posX, posY, 5, 0)
         self._texturePath = path.join("Assets", "Textures", "Blocks", "Unbreakable", "b_s.png")
+        self._blockType = "unbreakable"
 
 
 class Solo(Block):
     """Not connectable block daughter-class"""
 
     def __init__(self, posX, posY):
-        Block.__init__(self, posX, posY,1,0)
+        Block.__init__(self, posX, posY, 1, 0)
         self._texturePath = path.join("Assets", "Textures", "Blocks", "Solo", "b_s.png")
+        self._blockType = "solo"
 
 
 class Delayed(Block):
     """Timeout block daughter-class"""
 
     def __init__(self, posX, posY):
-        Block.__init__(self, posX, posY,1,0)
+        Block.__init__(self, posX, posY, 1, 0)
         self.__texturePath = path.join("Assets", "Textures", "Blocks", "Delayed", "b_s.png")
+        self._blockType = "delayed"
         self.__timeout = 84    # number of image for fadeout
+
 
 class Pill(Block):
     """Oxygen Pill"""
 
     def __init__(self, posX, posY):
-        Block.__init__(self, posX, posY,1,0)
+        Block.__init__(self, posX, posY, 1, 0)
         self._texturePath = path.join("Assets", "Textures", "Blocks", "Pill", "pill.png")
+        self._blockType = "pill"
